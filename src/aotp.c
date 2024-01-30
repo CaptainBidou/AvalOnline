30860b1c0d07a97d8bddd6b21855d3fc8adb96b2
tomas <tomas.salvado_robalo@ig2i.centralelille.fr>
2024-01-30 08:50:49 +0100

    AOTP requestHandler

--- src/aotp.c
+++ src/aotp.c
@@ -1,8 +1,5 @@
 #include "aotp.h"
 
-
-
-
 /**
  * \fn void connectHandler(socket_t *socket, aotp_request_t *requestData);
  * \brief Fonction de gestion de la connexion d'un client
@@ -10,8 +7,9 @@
  * \param requestData Requete de connexion du client
  * \note Cette fonction est appelee lors de la reception d'une requete de connexion
  * \warning lors de la creation d'un client, une allocation dynamique est effectuee
-*/
-void connectHandler(socket_t *socket, aotp_request_t *requestData) {
+ */
+void connectHandler(socket_t *socket, aotp_request_t *requestData)
+{
     // Creation d'un nouveau client
     client_t *client = malloc(sizeof(client_t));
     clientInit(client, requestData->client_id, requestData->pseudo, *socket);
@@ -25,29 +23,36 @@ void connectHandler(socket_t *socket, aotp_request_t *requestData) {
  * \fn void requestHandler(socket_t *socket, aotp_request_t *requestData)
  * \brief Fonction de gestion des requetes du protocole
  * Pour chaque requete il y un traitement specifique a effectuer
-*/
-void requestHandler(socket_t *socket, aotp_request_t *requestData) {
+ */
+void requestHandler(socket_t *socket, aotp_request_t *requestData, list_client_t *clients)
+{
     AOTP_REQUEST action = requestData->action;
-    switch (action) {
+    switch (action)
+    {
     case AOTP_CONNECT:
-        //connecte le client et l'ajoute a la liste des clients
+        // connecte le client et l'ajoute a la liste des clients
         connectHandler(socket, requestData);
         break;
-    
+
     case AOTP_DISCONNECT:
-        // supprime le client de la liste 
+        // supprime le client de la liste
         supprClient(&clients, requestData->client_id);
         break;
 
     case AOTP_CREATE_PARTY:
-        // TODO : Creer une partie et informer le serveur d'enregistrement
+        // Creer une partie et informer le serveur d'enregistrement
+        party_t *party = createParty(socket, requestData);
+
+        // ajoute la partie a la liste des parties
+        addParty(&parties, party);
+
         break;
 
     case AOTP_JOIN_PARTY:
         // TODO : Mettre en relation le client avec l'hote de la partie
-
+        connectClientToHost(socket, requestData);
         break;
-    
+
     default:
         break;
     }
@@ -58,21 +63,24 @@ void requestHandler(socket_t *socket, aotp_request_t *requestData) {
  * \brief Fonction de conversion d'une structure en requete
  * \param request Requete a convertir
  * \param buffer Buffer de conversion
- * 
-*/
-void struct2Request(aotp_request_t *request, char *buffer) {
+ *
+ */
+void struct2Request(aotp_request_t *request, char *buffer)
+{
     // On commence par écrire le header de la requête
     sprintf(buffer, "%d\r\n", request->action);
 
     // On écrit ensuite le body de la requête
 
     // écriture du client
-    if (request->client_id != 0) {
+    if (request->client_id != 0)
+    {
         sprintf(buffer, "%sclient_t %hd %s\r\n", buffer, request->client_id, request->pseudo);
     }
 
     // écriture de l'id de la partie
-    if (request->party_id != 0) {
+    if (request->party_id != 0)
+    {
         sprintf(buffer, "%sparty_id_t %d\r\n", buffer, request->party_id);
     }
 
@@ -84,8 +92,9 @@ void struct2Request(aotp_request_t *request, char *buffer) {
  * \brief Fonction de conversion d'une requete en structure
  * \param request la structure resultante de la conversion
  * \param buffer requete a convertir
-*/
-void request2Struct(char *buffer, aotp_request_t *request) {
+ */
+void request2Struct(char *buffer, aotp_request_t *request)
+{
     // Copie du buffer dans une variable locale
     char *bufferCopy = malloc(strlen(buffer) * sizeof(char));
     char *saveptr;
@@ -97,21 +106,23 @@ void request2Struct(char *buffer, aotp_request_t *request) {
     // Traitement du body
     char *body = strtok_r(NULL, AOTP_EMPTY_LINE, &saveptr);
 
-    while (body != NULL && body[0] != '\0') {
-        if (strncmp(body, "client_t", strlen("client_t")) == 0) {
+    while (body != NULL && body[0] != '\0')
+    {
+        if (strncmp(body, "client_t", strlen("client_t")) == 0)
+        {
             sscanf(body, "client_t %hd %20s", &request->client_id, request->pseudo);
         }
 
-        if (strncmp(body, "party_id_t", strlen("party_id_t")) == 0) {
+        if (strncmp(body, "party_id_t", strlen("party_id_t")) == 0)
+        {
             sscanf(body, "party_id_t %d", &request->party_id);
         }
 
-        // TODO : Ajouter les autres cas 
+        // TODO : Ajouter les autres cas
         // Passe à la ligne suivante
         body = strtok_r(NULL, "\r\n", &saveptr);
     }
 }
-
 
 /* ------------------------------------------------------------------------ */
 /*            M A N I P U L A T I O N    D E    C L I E N T S               */
@@ -124,13 +135,13 @@ void request2Struct(char *buffer, aotp_request_t *request) {
  * \param id Identifiant du client
  * \param pseudo Pseudo du client
  * \param socket Socket du client
-*/
-void clientInit(client_t *client, short id, char *pseudo, socket_t socket) {
+ */
+void clientInit(client_t *client, short id, char *pseudo, socket_t socket)
+{
     client->id = id;
     strcpy(client->pseudo, pseudo);
     client->socket = socket;
 }
-
 
 /* ------------------------------------------------------------------------ */
 /*            M A N I P U L A T I O N    D E    P A R T I E S               */
@@ -143,16 +154,17 @@ void clientInit(client_t *client, short id, char *pseudo, socket_t socket) {
  * \param id Identifiant de la partie
  * \param host Hote de la partie
  * \param state Etat de la partie
-*/
-void partyInit(party_t *party, party_id_t id, client_t *host, party_state_t state) {
+ */
+void partyInit(party_t *party, party_id_t id, client_t *host, party_state_t state)
+{
     party->id = id;
     // Récupération de l'adresse IP de l'hote
-    if(host != NULL) {
+    if (host != NULL)
+    {
         strcpy(party->host_ip, inet_ntoa(host->socket.localAddr.sin_addr));
         party->host_port = host->socket.localAddr.sin_port;
     }
     party->state = state;
-
 }
 
 /**
@@ -160,8 +172,9 @@ void partyInit(party_t *party, party_id_t id, client_t *host, party_state_t stat
  * \brief Fonction de conversion d'une partie en chaine de caracteres
  * \param party Partie a convertir
  * \param buffer Chaine de caracteres resultante
-*/
-void partyToString(party_t *party, char *buffer) {
+ */
+void partyToString(party_t *party, char *buffer)
+{
     sprintf(buffer, "%d %s %d %d\r\n", party->id, party->host_ip, party->host_port, party->state);
 }
 
@@ -170,9 +183,10 @@ void partyToString(party_t *party, char *buffer) {
  * \brief Fonction de conversion d'une chaine de caracteres en partie
  * \param buffer Chaine de caracteres a convertir
  * \param party Partie resultante
-*/
-void stringToParty(char *buffer, party_t *party) {
-    sscanf(buffer, "%d %s %hd %d\r\n", &party->id, party->host_ip, &party->host_port, (int *) &party->state);
+ */
+void stringToParty(char *buffer, party_t *party)
+{
+    sscanf(buffer, "%d %s %hd %d\r\n", &party->id, party->host_ip, &party->host_port, (int *)&party->state);
 }
 
 /* ------------------------------------------------------------------------ */
@@ -184,8 +198,9 @@ void stringToParty(char *buffer, party_t *party) {
  * \brief Fonction de conversion d'un coup en chaine de caracteres
  * \param coup Coup a convertir
  * \param buffer Chaine de caracteres resultante
-*/
-void coupToString(coup_t *coup, char *buffer) {
+ */
+void coupToString(coup_t *coup, char *buffer)
+{
     // Un coup est represente par deux caracteres : origine et destination
     sprintf(buffer, "%c:%c", coup->origine, coup->destination);
 }
@@ -195,8 +210,9 @@ void coupToString(coup_t *coup, char *buffer) {
  * \brief Fonction de conversion d'une chaine de caracteres en coup
  * \param buffer Chaine de caracteres a convertir
  * \param coup Coup resultante
-*/
-void stringToCoup(char *buffer, coup_t *coup) {
+ */
+void stringToCoup(char *buffer, coup_t *coup)
+{
     sscanf(buffer, "%c %c", &coup->origine, &coup->destination);
 }
 
@@ -209,8 +225,9 @@ void stringToCoup(char *buffer, coup_t *coup) {
  * \brief Fonction de conversion d'une evolution en chaine de caracteres
  * \param evolution Evolution a convertir
  * \param buffer Chaine de caracteres resultante
-*/
-void evolutionToString(evolution_t *evolution, char *buffer) {
+ */
+void evolutionToString(evolution_t *evolution, char *buffer)
+{
     sprintf(buffer, "%c %c %c %c", evolution->bonusJ, evolution->malusJ, evolution->bonusR, evolution->malusR);
 }
 
@@ -219,8 +236,9 @@ void evolutionToString(evolution_t *evolution, char *buffer) {
  * \brief Fonction de conversion d'une chaine de caracteres en evolution
  * \param buffer Chaine de caracteres a convertir
  * \param evolution Evolution resultante
-*/
-void stringToEvolution(char *buffer, evolution_t *evolution) {
+ */
+void stringToEvolution(char *buffer, evolution_t *evolution)
+{
     sscanf(buffer, "%c %c %c %c", &evolution->bonusJ, &evolution->malusJ, &evolution->bonusR, &evolution->malusR);
 }
 
@@ -228,8 +246,9 @@ void stringToEvolution(char *buffer, evolution_t *evolution) {
  * \fn list_client_t *initClientList(client_t *client);
  * \brief Fonction d'initialisation d'une liste de clients
  * \param list Liste de clients a initialiser
-*/
-list_client_t *initClientList(client_t *client) {
+ */
+list_client_t *initClientList(client_t *client)
+{
     list_client_t *list = malloc(sizeof(list_client_t));
     list->client = client;
     list->next = NULL;
@@ -241,16 +260,21 @@ list_client_t *initClientList(client_t *client) {
  * \brief Fonction d'ajout d'un client a une liste de clients
  * \param list Liste de clients
  * \param client Client a ajouter
-*/
-void addClient(list_client_t **head, client_t *client) {
+ */
+void addClient(list_client_t **head, client_t *client)
+{
     list_client_t *newClient = malloc(sizeof(list_client_t));
     newClient->client = client;
     newClient->next = NULL;
-    if (*head == NULL) { 
+    if (*head == NULL)
+    {
         *head = newClient;
-    } else {
+    }
+    else
+    {
         list_client_t *current = *head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
+        {
             current = current->next;
         }
         current->next = newClient;
@@ -262,15 +286,21 @@ void addClient(list_client_t **head, client_t *client) {
  * \brief Fonction de suppression d'un client d'une liste de clients
  * \param list Liste de clients
  * \param client Client a supprimer
-*/
-void removeClient(list_client_t **head, client_t *client) {
+ */
+void removeClient(list_client_t **head, client_t *client)
+{
     list_client_t *current = *head;
     list_client_t *previous = NULL;
-    while (current != NULL) {
-        if (current->client->id == client->id) {
-            if (previous == NULL) {
+    while (current != NULL)
+    {
+        if (current->client->id == client->id)
+        {
+            if (previous == NULL)
+            {
                 *head = current->next;
-            } else {
+            }
+            else
+            {
                 previous->next = current->next;
             }
             free(current);
@@ -278,7 +308,7 @@ void removeClient(list_client_t **head, client_t *client) {
         }
         previous = current;
         current = current->next;
-    }   
+    }
 }
 
 /**
@@ -288,21 +318,22 @@ void removeClient(list_client_t **head, client_t *client) {
  * \param code Code de retour de la reponse
  * \param parties Parties a retourner (optionnel)
  * \param position Position a retourner (optionnel)
-*/
-void initResponse(aotp_response_t *response, AOTP_RESPONSE code, list_party_t *parties, position_t *position) {
+ */
+void initResponse(aotp_response_t *response, AOTP_RESPONSE code, list_party_t *parties, position_t *position)
+{
     response->code = code;
     response->parties = parties;
     response->position = position;
 }
-
 
 /**
  * \fn void struct2Response(aotp_response_t *response, char *buffer);
  * \brief Fonction de conversion d'une structure en reponse sous le format aotp
  * \param response Reponse a convertir
  * \param buffer Buffer de conversion
-*/
-void struct2Response(aotp_response_t *response, char *buffer) {
+ */
+void struct2Response(aotp_response_t *response, char *buffer)
+{
     // On commence par écrire le header de la requête
     sprintf(buffer, "%d\r\n", response->code);
     // Gestion des paramètre optionnels
@@ -312,7 +343,8 @@ void struct2Response(aotp_response_t *response, char *buffer) {
     // Ecriture des parties
     // Parcours de la liste des parties
     list_party_t *current = response->parties;
-    while (current != NULL) {
+    while (current != NULL)
+    {
         // Conversion de la partie en chaine de caractères
         char *partie = malloc(sizeof(char) * 100);
         partyToString(current->party, partie);
@@ -327,7 +359,8 @@ void struct2Response(aotp_response_t *response, char *buffer) {
     position_t *position = response->position;
     sprintf(buffer, "%s%d %d\r\n", buffer, position->trait, position->numCoup);
     // Ecriture des colonnes
-    for (int i = 0; i < NBCASES; i++) {
+    for (int i = 0; i < NBCASES; i++)
+    {
         sprintf(buffer, "%s%d %d\r\n", buffer, position->cols[i].nb, position->cols[i].couleur);
     }
     // Ecriture de la ligne vide
@@ -339,9 +372,10 @@ void struct2Response(aotp_response_t *response, char *buffer) {
  * \brief Fonction de conversion d'une reponse sous le format aotp en structure
  * \param buffer Reponse a convertir
  * \param response Structure resultante
- * 
-*/
-void response2Struct(char *buffer, aotp_response_t *response) {
+ *
+ */
+void response2Struct(char *buffer, aotp_response_t *response)
+{
     // Copie du buffer dans une variable locale
     char *bufferCopy = malloc(strlen(buffer) * sizeof(char));
     char *saveptr;
@@ -350,22 +384,26 @@ void response2Struct(char *buffer, aotp_response_t *response) {
     // Recuperation du header
     char *header = strtok_r(bufferCopy, "\r\n", &saveptr);
     sscanf(header, "%d", (int *)&response->code);
-    
+
     // Traitement des paramètres optionnels
     char *body = strtok_r(NULL, "\r\n", &saveptr);
-    while (body != NULL) {
-        if(strcmp(body, "::") == 0) {
+    while (body != NULL)
+    {
+        if (strcmp(body, "::") == 0)
+        {
             break;
         }
-        // TODO : Ajouter les autres cas 
+        // TODO : Ajouter les autres cas
         body = strtok_r(NULL, "\r\n", &saveptr);
     }
 
     // Traitement des parties
     body = strtok_r(NULL, "\r\n", &saveptr);
-    while (body != NULL) {
+    while (body != NULL)
+    {
         printf("body: %s\n", body);
-        if(strcmp(body, "::") == 0) {
+        if (strcmp(body, "::") == 0)
+        {
             break;
         }
         // Conversion de la chaine de caractères en partie
@@ -384,7 +422,8 @@ void response2Struct(char *buffer, aotp_response_t *response) {
     position_t *position = malloc(sizeof(position_t));
     sscanf(body, "%hhd %hhd", &position->trait, &position->numCoup);
     // Traitement des colonnes
-    for (int i = 0; i < NBCASES; i++) {
+    for (int i = 0; i < NBCASES; i++)
+    {
         body = strtok_r(NULL, "\r\n", &saveptr);
         sscanf(body, "%hhd %hhd", &position->cols[i].nb, &position->cols[i].couleur);
     }
@@ -397,8 +436,9 @@ void response2Struct(char *buffer, aotp_response_t *response) {
  * \brief Fonction d'initialisation d'une liste de parties
  * \param list Liste de parties a initialiser
  * \param party Partie a ajouter (optionnel)
-*/
-list_party_t *initPartyList(party_t *party) {
+ */
+list_party_t *initPartyList(party_t *party)
+{
     list_party_t *list = malloc(sizeof(list_party_t));
     list->party = party;
     list->next = NULL;
@@ -410,14 +450,19 @@ list_party_t *initPartyList(party_t *party) {
  * \brief Fonction d'ajout d'une partie a une liste de parties
  * \param head Liste de parties
  * \param party Partie a ajouter
-*/
-void addParty(list_party_t **head, party_t *party) {
+ */
+void addParty(list_party_t **head, party_t *party)
+{
     list_party_t *newParty = initPartyList(party);
-    if (*head == NULL) { 
+    if (*head == NULL)
+    {
         *head = newParty;
-    } else {
+    }
+    else
+    {
         list_party_t *current = *head;
-        while (current->next != NULL) {
+        while (current->next != NULL)
+        {
             current = current->next;
         }
         current->next = newParty;
@@ -429,15 +474,21 @@ void addParty(list_party_t **head, party_t *party) {
  * \brief Fonction de suppression d'une partie d'une liste de parties
  * \param list Liste de parties
  * \param party Partie a supprimer
-*/
-void removeParty(list_party_t **list, party_t *party) {
+ */
+void removeParty(list_party_t **list, party_t *party)
+{
     list_party_t *current = *list;
     list_party_t *previous = NULL;
-    while (current != NULL) {
-        if (current->party->id == party->id) {
-            if (previous == NULL) {
+    while (current != NULL)
+    {
+        if (current->party->id == party->id)
+        {
+            if (previous == NULL)
+            {
                 *list = current->next;
-            } else {
+            }
+            else
+            {
                 previous->next = current->next;
             }
             free(current);
@@ -445,7 +496,7 @@ void removeParty(list_party_t **list, party_t *party) {
         }
         previous = current;
         current = current->next;
-    }   
+    }
 }
 
 /**
@@ -453,16 +504,22 @@ void removeParty(list_party_t **list, party_t *party) {
  * \brief Fonction de suppression d'un client d'une liste de clients
  * \param clients Liste des clients
  * \param id Identifiant du client a supprimer
-*/
-void supprClient(list_client_t **clients, short id) {
+ */
+void supprClient(list_client_t **clients, short id)
+{
 
     list_client_t *current = clients;
     list_client_t *previous = NULL;
-    while (current != NULL) {
-        if (current->client->id == id) {
-            if (previous == NULL) {
+    while (current != NULL)
+    {
+        if (current->client->id == id)
+        {
+            if (previous == NULL)
+            {
                 clients = current->next;
-            } else {
+            }
+            else
+            {
                 previous->next = current->next;
             }
             free(current);
@@ -470,5 +527,29 @@ void supprClient(list_client_t **clients, short id) {
         }
         previous = current;
         current = current->next;
-    }   
+    }
+}
+
+/**
+ * \fn party_t *createParty(socket, requestData);
+ * \brief Fonction de création d'une partie
+ * \param socket Socket du client
+ * \param requestData Requete de connexion du client
+ * \return party_t *party
+ */
+party_t *createParty(socket_t *socket, aotp_request_t *requestData)
+{
+    // TODO : créer la partie
+    return NULL;
+}
+
+/**
+ * \fn void connectClientToHost(socket, requestData);
+ * \brief Fonction de création d'une partie
+ * \param socket Socket du client
+ * \param requestData Requete de connexion du client
+ */
+void connectClientToHost(socket_t *socket, aotp_request_t *requestData)
+{
+    // TODO : connecter le client à l'hote
 }
